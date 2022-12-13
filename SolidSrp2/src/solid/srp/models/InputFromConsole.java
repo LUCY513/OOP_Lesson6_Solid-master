ce547a54a93fba406368e80fc8dfec023d98d9a5
LUCY513 <gysuyagoasdg@mail.ru>
2022-12-13 12:10:11 +0300

    Create Branch-master

---
+++ SolidSrp2/src/solid/srp/models/InputFromConsole.java
@@ -0,0 +1,20 @@
+package solid.srp.models;
+
+import java.util.Scanner;
+
+public class InputFromConsole {
+
+    public Order inputFromConsole(){
+        return new Order(
+                prompt("Client name: "),
+                prompt("Product: "),
+                Integer.parseInt(prompt("Quantity: ")),
+                Integer.parseInt(prompt("Price: "))
+                );
+    }
+    private String prompt(String message) {
+        Scanner in = new Scanner(System.in);
+        System.out.print(message);
+        return in.nextLine();
+    }
+}
